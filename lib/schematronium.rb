ca370c14706eb6418e0e92a4487ba677fe1dd340
Dave Mayo <dave_mayo@harvard.edu>
2015-07-31 11:11:59 -0400

    Doc updates

--- lib/schematronium.rb
+++ lib/schematronium.rb
@@ -2,6 +2,7 @@ require 'saxon-xslt'
 require 'nokogiri' # Used for output parsing - there doesn't seem to be a clean way to manipulate Saxon::XML documents
 
 class Schematronium
+  # Helper method returning stages of iso_schematron XSLT transform
   def iso_file(fname)
     Saxon.XSLT(File.open(File.join(File.dirname(File.expand_path(__FILE__)), 'iso-schematron-xslt2', fname)))
   end
@@ -22,6 +23,9 @@ class Schematronium
                    end
                  end
 
+    # Run schematron through each stage of the iso_schematron pipeline
+    #    then stringify the final result because Saxon.XSLT can't take
+    #    an XML doc as input
     @sch_script = Saxon.XSLT(
       stages.reduce(schematron) do |result, stage|
         stage.transform(result)
@@ -29,6 +33,7 @@ class Schematronium
     )
   end
 
+  # Run schematron over xml document, returning NodeSet of errors
   def check(xml)
     xml = Nokogiri::XML(@sch_script.transform( Saxon.XML(xml)).to_s)
     xml.remove_namespaces!
