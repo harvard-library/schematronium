f74cff1c9c6876d8398e63868666f1489172ee50
Dave Mayo <dave_mayo@harvard.edu>
2015-09-02 16:29:35 -0400

    Updated tests to cover phase support

--- test/test_data/schematron/test.sch
+++ test/test_data/schematron/test.sch
@@ -1,6 +1,9 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <schema xmlns="http://purl.oclc.org/dsdl/schematron" queryBinding="xslt2">
-  <pattern>
+  <phase id="testphase">
+    <active pattern="b" />
+  </phase>
+<pattern id="a">
     <rule context="*:test">
       <report test="*:testable">
         There should be 'testable' elements within 'test's.
@@ -9,5 +12,10 @@
     <rule context="*:test/*:testable">
       <assert test="@test-attr">Testables must have have the 'test-attr' attribute.</assert>
     </rule>
-  </pattern>
+</pattern>
+<pattern id="b">
+    <rule context="*:test[1]">
+      <assert test="@test-attr">Testables must have have the 'test-attr' attribute.</assert>
+    </rule>
+</pattern>
 </schema>
--- test/test_schematronium.rb
+++ test/test_schematronium.rb
@@ -8,15 +8,18 @@ class SchematroniumTest < MiniTest::Test
     File.join(File.expand_path(File.dirname(__FILE__)), 'test_data', *path_segments)
   end
 
-  def setup
-    @stron = Schematronium.new(td('schematron', 'test.sch'))
-  end
-
   def test_check_with_file
-    results = @stron.check(File.open(td('xml', 'test.xml')))
+    stron = Schematronium.new(td('schematron', 'test.sch'))
+    results = stron.check(File.open(td('xml', 'test.xml')))
     results.remove_namespaces!
-    assert_equal 1, results.xpath("//failed-assert").count, "Expects one failure"
+    assert_equal 2, results.xpath("//failed-assert").count, "Expects two failures"
     assert_equal 2, results.xpath("//successful-report").count, "Expects two reports"
   end
 
+  def test_check_with_phase
+    stron = Schematronium.new(td('schematron', 'test.sch'), "'testphase'")
+    results = stron.check(File.open(td('xml', 'test.xml')))
+    results.remove_namespaces!
+    assert_equal 1, results.xpath("//failed-assert").count, "Expects one failure"
+  end
 end
